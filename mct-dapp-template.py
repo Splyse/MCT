a0ab45ab579b40994469a97c3cda05bb19ebafdb
hal0x2328 <31932411+hal0x2328@users.noreply.github.com>
2018-05-16 10:35:50 -0400

    Updated with privatenet MCT scripthash

--- mct-dapp-template.py
+++ mct-dapp-template.py
@@ -22,9 +22,15 @@ from boa.interop.Neo.App import RegisterAppCall
 
 OWNER = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
-MCT_SCRIPTHASH = b'?\xbc`|\x12\xc2\x87642$\xa4\xb4\xd8\xf5\x13\xa5\xc2|\xa8'
+# mainnet
+#MCT_SCRIPTHASH = b'?\xbc`|\x12\xc2\x87642$\xa4\xb4\xd8\xf5\x13\xa5\xc2|\xa8'
+# privatenet
+MCT_SCRIPTHASH = b'\x8dKL\x14V4\x17\xc6\x91\x91\xe0\x8b\xe0\xb8m\xdc\xb4\xbc\x86\xc1'
 
-MCTContract = RegisterAppCall('a87cc2a513f5d8b4a42432343687c2127c60bc3f', 'operation', 'args')
+# mainnet
+#MCTContract = RegisterAppCall('a87cc2a513f5d8b4a42432343687c2127c60bc3f', 'operation', 'args')
+# privatenet
+MCTContract = RegisterAppCall('c186bcb4dc6db8e08be09191c6173456144c4b8d', 'operation', 'args')
 
 def Main(operation, args):
 
